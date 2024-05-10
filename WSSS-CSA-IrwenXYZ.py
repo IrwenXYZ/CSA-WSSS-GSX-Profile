1661acc32223e80371f42ce0924045a686a359e7
IrwenXYZ <hello@irwen.xyz>
2024-05-11 04:08:22 +0800

    Update WSSS-CSA-IrwenXYZ.py
    per gate offset settings

--- WSSS-CSA-IrwenXYZ.py
+++ WSSS-CSA-IrwenXYZ.py
@@ -24,10 +24,11 @@ eastCargo = CustomizedName("East Cargo Apron (600-604) | Stand #", 6)
 acehub = CustomizedName("Acehub (611-612) | Stand #", 7)
 parking = CustomizedName("Parking | Stand #", 0)
 
-
+# A320 - 7.5m
 @AlternativeStopPositions
-def ChangiOffsets(aircraftData):
+def westCargoOffset(aircraftData):
     offset = {
+        310: 5,
         318: 7.5,
         319: 7.5,
         320: 7.5,
@@ -47,9 +48,128 @@ def ChangiOffsets(aircraftData):
         except:
             return Distance.fromMeters(0)
 
+@AlternativeStopPositions
+def eastCargoOffset(aircraftData):
+    offset = {
+        310: 5,
+        318: 7.5,
+        319: 7.5,
+        320: 7.5,
+        330: 3.5,
+        747: 3.5,
+    }
+    
+    offset787 = {
+        9: 3.2,
+        10: 3.5,
+    }
+    if aircraftData.idMajor == 787:
+        return Distance.fromMeters(offset787.get(aircraftData.idMinor))
+    else:
+        try:
+            return Distance.fromMeters(offset.get(aircraftData.idMajor))
+        except:
+            return Distance.fromMeters(0)
+
+
+
+# A320 - 7.4m
+# @AlternativeStopPositions
+# def northRemoteOffset(aircraftData):
+#     offset = {
+#         310: 4.9,
+#         318: 7.4,
+#         319: 7.4,
+#         320: 7.4,
+#         330: 3.4,
+#         747: 3.4,
+#     }
+    
+#     offset787 = {
+#         9: 3.1,
+#         10: 3.4,
+#     }
+#     if aircraftData.idMajor == 787:
+#         return Distance.fromMeters(offset787.get(aircraftData.idMinor))
+#     else:
+#         try:
+#             return Distance.fromMeters(offset.get(aircraftData.idMajor))
+#         except:
+#             return Distance.fromMeters(0)
+
+# A320 - 7.3m
+@AlternativeStopPositions
+def northRemoteOffset(aircraftData):
+    offset = {
+        310: 4.8,
+        318: 7.3,
+        319: 7.3,
+        320: 7.3,
+        330: 3.3,
+        747: 3.3,
+    }
+    
+    offset787 = {
+        9: 3,
+        10: 3.3,
+    }
+    if aircraftData.idMajor == 787:
+        return Distance.fromMeters(offset787.get(aircraftData.idMinor))
+    else:
+        try:
+            return Distance.fromMeters(offset.get(aircraftData.idMajor))
+        except:
+            return Distance.fromMeters(0)
+
+@AlternativeStopPositions
+def acehubOffset(aircraftData):
+    offset = {
+        310: 4.8,
+        318: 7.3,
+        319: 7.3,
+        320: 7.3,
+        330: 3.3,
+        747: 3.3,
+    }
+    
+    offset787 = {
+        9: 3,
+        10: 3.3,
+    }
+    if aircraftData.idMajor == 787:
+        return Distance.fromMeters(offset787.get(aircraftData.idMinor))
+    else:
+        try:
+            return Distance.fromMeters(offset.get(aircraftData.idMajor))
+        except:
+            return Distance.fromMeters(0)
+
+@AlternativeStopPositions
+def terminalOffset(aircraftData):
+    offset = {
+        310: 4.8,
+        318: 7.3,
+        319: 7.3,
+        320: 7.3,
+        330: 3.3,
+        747: 3.3,
+    }
+    
+    offset787 = {
+        9: 3,
+        10: 3.3,
+    }
+    if aircraftData.idMajor == 787:
+        return Distance.fromMeters(offset787.get(aircraftData.idMinor))
+    else:
+        try:
+            return Distance.fromMeters(offset.get(aircraftData.idMajor))
+        except:
+            return Distance.fromMeters(0)
+
 parkings = {
     E_PARKING : {
-        None : (acehub, ),
+        None : (acehub, acehubOffset),
     },
     GATE : {
         None : (CustomizedName("Terminal 4 G Gates| Gate G#", 4), )
@@ -64,10 +184,10 @@ parkings = {
         None : (terminal1Charlie, )
     },
     GATE_D : {
-        None : (terminal1Delta, )
+        None : (terminal1Delta, terminalOffset)
     },
     GATE_E : {
-        None : (terminal2Echo, )
+        None : (terminal2Echo, terminalOffset)
     },
     GATE_F : {
         None : (terminal2Foxtrot, )
@@ -76,50 +196,50 @@ parkings = {
         None : (terminal4Golf, )
     },
     PARKING : {
-        200 : (eastRemote, ChangiOffsets),
-        "200L" : (eastRemote, ChangiOffsets),
-        201 : (eastRemote, ChangiOffsets),
-        202 : (eastRemote, ChangiOffsets),
-        203 : (eastRemote, ChangiOffsets),
-        205 : (southEastRemote, ChangiOffsets),
-        206 : (southEastRemote, ChangiOffsets),
-        207 : (southEastRemote, ChangiOffsets),
-        208 : (southEastRemote, ChangiOffsets),
-        209 : (southEastRemote, ChangiOffsets),
-        300 : (northRemote, ChangiOffsets),
-        301 : (northRemote, ChangiOffsets),
-        302 : (northRemote, ChangiOffsets),
-        303 : (northRemote, ChangiOffsets),
-        304 : (northRemote, ChangiOffsets),
-        305 : (northRemote, ChangiOffsets),
-        306 : (northRemote, ChangiOffsets),
-        307 : (northRemote, ChangiOffsets),
-        308 : (northRemote, ChangiOffsets),
-        309 : (northRemote, ChangiOffsets),
-        310 : (northRemote, ChangiOffsets),
-        502 : (westCargo, ChangiOffsets),
-        503 : (westCargo, ChangiOffsets),
-        504 : (westCargo, ChangiOffsets),
-        505 : (westCargo, ChangiOffsets),
-        506 : (westCargo, ChangiOffsets),
-        507 : (westCargo, ChangiOffsets),
-        508 : (westCargo, ChangiOffsets),
-        509 : (westCargo, ChangiOffsets),
-        510 : (westCargo, ChangiOffsets),
-        511 : (westCargo, ChangiOffsets),
-        512 : (westCargo, ChangiOffsets),
-        513 : (westCargo, ChangiOffsets),
-        514 : (westCargo, ChangiOffsets),
-        515 : (westCargo, ChangiOffsets),
-        516 : (westCargo, ChangiOffsets),
-        517 : (westCargo, ChangiOffsets),
-        600 : (eastCargo, ChangiOffsets),
-        601 : (eastCargo, ChangiOffsets),
-        602 : (eastCargo, ChangiOffsets),
-        603 : (eastCargo, ChangiOffsets),
-        604 : (eastCargo, ChangiOffsets),
+        200 : (eastRemote, ),
+        "200L" : (eastRemote, ),
+        201 : (eastRemote, ),
+        202 : (eastRemote, ),
+        203 : (eastRemote, ),
+        205 : (southEastRemote, ),
+        206 : (southEastRemote, ),
+        207 : (southEastRemote, ),
+        208 : (southEastRemote, ),
+        209 : (southEastRemote, ),
+        300 : (northRemote, northRemoteOffset),
+        301 : (northRemote, northRemoteOffset),
+        302 : (northRemote, northRemoteOffset),
+        303 : (northRemote, northRemoteOffset),
+        304 : (northRemote, northRemoteOffset),
+        305 : (northRemote, northRemoteOffset),
+        306 : (northRemote, northRemoteOffset),
+        307 : (northRemote, northRemoteOffset),
+        308 : (northRemote, northRemoteOffset),
+        309 : (northRemote, northRemoteOffset),
+        310 : (northRemote, northRemoteOffset),
+        502 : (westCargo, westCargoOffset),
+        503 : (westCargo, westCargoOffset),
+        504 : (westCargo, westCargoOffset),
+        505 : (westCargo, westCargoOffset),
+        506 : (westCargo, westCargoOffset),
+        507 : (westCargo, westCargoOffset),
+        508 : (westCargo, westCargoOffset),
+        509 : (westCargo, westCargoOffset),
+        510 : (westCargo, westCargoOffset),
+        511 : (westCargo, westCargoOffset),
+        512 : (westCargo, westCargoOffset),
+        513 : (westCargo, westCargoOffset),
+        514 : (westCargo, westCargoOffset),
+        515 : (westCargo, westCargoOffset),
+        516 : (westCargo, westCargoOffset),
+        517 : (westCargo, westCargoOffset),
+        600 : (eastCargo, eastCargoOffset),
+        601 : (eastCargo, eastCargoOffset),
+        602 : (eastCargo, eastCargoOffset),
+        603 : (eastCargo, eastCargoOffset),
+        604 : (eastCargo, eastCargoOffset),
     },
     0 : {
-        0 : (CustomizedName("Terminal 2 F Gates | Gate F36", 2), ChangiOffsets),
+        0 : (CustomizedName("Terminal 2 F Gates | Gate F36", 2), ),
     },
 }
