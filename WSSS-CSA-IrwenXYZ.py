264610ab0707119ec2fa372c1a58878756c711a9
IrwenXYZ <hello@irwen.xyz>
2025-02-19 09:46:51 +0800

    formatting

--- WSSS-CSA-IrwenXYZ.py
+++ WSSS-CSA-IrwenXYZ.py
@@ -38,13 +38,13 @@ def eastWestCargoOffset(aircraftData):
     777: 8.9,
     380: 9
     }
-    
+
     offset787 = {
         8: 8.7,
         9: 8.7,
         10: 9,
     }
-    
+
     if aircraftData.idMajor == 787:
         return Distance.fromMeters(offset787.get(aircraftData.idMinor))
     else:
@@ -69,7 +69,7 @@ def terminalOffset(aircraftData):
     }
     
     offset787 = {
-        8: 8.5,
+	8: 8.5,
         9: 8.5,
         10: 8.8,
     }
